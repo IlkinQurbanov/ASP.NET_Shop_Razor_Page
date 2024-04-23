563d9c936438c06e6518e2b98b4ad8678ab2cc34
Ilkin Qurbanov <ilkinlikus@gmail.com>
2024-04-23 15:16:02 +0400

    Creating required fields

--- ShopMvc/Pages/Contact.cshtml.cs
+++ ShopMvc/Pages/Contact.cshtml.cs
@@ -7,6 +7,47 @@ namespace ShopMvc.Pages
     {
         public void OnGet()
         {
+
+        }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Subject { get; set; } = "";
+        public string Message { get; set; } = "";
+
+        public string SuccessMessage { get; set; } = "";
+        public string ErrorMessage { get; set; } = "";
+
+        public void OnPost()
+        {
+            FirstName = Request.Form["firstname"];
+            LastName = Request.Form["lastname"];
+            Email = Request.Form["email"];
+            Phone = Request.Form["phone"];
+            Subject = Request.Form["subject"];
+            Message = Request.Form["message"];
+
+            //check if any required field is emtpy
+            if (FirstName.Length == 0 || LastName.Length == 0 || Email.Length == 0 || Phone.Length == 0|| 
+                Subject.Length == 0 || Message.Length == 0)
+            {
+                //Error
+                ErrorMessage = "Please fill all required fields";
+                return;
+            }
+            //Add this message to database
+
+            //Send Confirmation Email tp the client
+
+            SuccessMessage = "Your message has been received correctly";
+
+            FirstName = "";
+            LastName = "";
+            Email = "";
+            Phone = "";
+            Subject = "";
+            Message = "";
         }
     }
 }
